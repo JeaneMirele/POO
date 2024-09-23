eaa8c6dac4aff3bb68dedd8daf87b05e2b67edb7
JeaneMirele <jeanemirele7@gmail.com>
2024-09-23 08:13:47 -0300

    Change: main method location

--- src/Elevator.java
+++ src/Elevator.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class Elevator{
     private int andar;
     private int totalAndares;
@@ -47,66 +45,6 @@ public class Elevator{
         System.out.println("Andar atual:"+ this.andar);
         System.out.println("Capacidade total:"+ this.capacidade);
         System.out.println("Pessoas presentes:" + this.pessoas);
-
-    }
-
-    public static void main(String[] args) {
-        Elevator elevator = new Elevator();
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Digite a capacidade do elevador: ");
-        int capacidade = sc.nextInt();
-
-        System.out.println("Digite o total de andares do prédio (sem contar o térreo): ");
-        int totalAndares = sc.nextInt();
-
-        elevator.inicializa(capacidade, totalAndares);
-
-        int escolha;
-        do {
-            System.out.println("MENU");
-            System.out.println("1- Ver status do elevador");
-            System.out.println("2-Adicionar pessoa ao elevador");
-            System.out.println("3- Remover pessoa do elevador");
-            System.out.println("4- Subir de andar");
-            System.out.println("5- Descer de andar");
-            System.out.println("6- Sair do programa");
-
-
-            escolha = sc.nextInt();
-
-            switch (escolha) {
-
-                case 1:
-                    elevator.status();
-                    break;
-
-                case 2:
-                    elevator.entra();
-                    break;
-
-                case 3:
-                    elevator.sai();
-                    break;
-
-                case 4:
-                    elevator.subir();
-                    break;
-
-                case 5:
-                    elevator.descer();
-                    break;
-
-                case 6:
-                    sc.close();
-                    return;
-
-                default:
-                    System.out.println("Escolha Inválida!");
-            }
-
-        } while (escolha!=6);
-
 
     }
 
